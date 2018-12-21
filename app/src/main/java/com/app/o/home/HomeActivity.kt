d48989aef4d188ff6e8b8a4d49ef66037473b305
Reza Rachman <reza.rachman@traveloka.com>
2018-12-21 08:18:40 +0700

    Add icons for action bar menu.

--- app/src/main/java/com/app/o/home/HomeActivity.kt
+++ app/src/main/java/com/app/o/home/HomeActivity.kt
@@ -2,6 +2,8 @@ package com.app.o.home
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import com.app.o.R
 
 class HomeActivity : AppCompatActivity() {
@@ -10,4 +12,24 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.home_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.action_search -> {
+            true
+        }
+
+        R.id.action_friend -> {
+            true
+        }
+
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+
 }
