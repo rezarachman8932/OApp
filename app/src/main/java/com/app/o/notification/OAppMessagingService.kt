c66b7a28afb74145b1f52091e9805be1a94a0b49
Reza Rachman <reza.rachman@traveloka.com>
2019-03-03 18:18:18 +0700

    Fix notification service.

--- app/src/main/java/com/app/o/notification/OAppMessagingService.kt
+++ app/src/main/java/com/app/o/notification/OAppMessagingService.kt
@@ -14,6 +14,8 @@ import com.app.o.home.HomeActivity
 import com.app.o.shared.util.OAppUserUtil
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class OAppMessagingService : FirebaseMessagingService() {
@@ -32,39 +34,44 @@ class OAppMessagingService : FirebaseMessagingService() {
         Log.i("onMessageReceived", remoteMessage.from)
 
         if (remoteMessage.data.isNotEmpty()) {
-            Log.i("RemoteMessage", "Message Data Payload: " + remoteMessage.data)
+            Log.i("Data", "Message Data Payload: " + remoteMessage.data)
         }
 
         if (remoteMessage.notification != null) {
             Log.i("Notification", "Message Notification Body: " + remoteMessage.notification?.body)
         }
 
-        sendNotification(remoteMessage)
+        sendNotification()
     }
 
-    private fun sendNotification(remoteMessage: RemoteMessage) {
+    private fun sendNotification() {
         val intent = Intent(this, HomeActivity::class.java)
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
 
         val pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT)
-        val channelId = "com.app.o.notification"
+        val channelId = getString(R.string.text_notification_channel_id)
+        val channelName = getString(R.string.text_notification_channel_name)
         val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
         val notificationBuilder = NotificationCompat.Builder(this, channelId)
                 .setSmallIcon(R.drawable.ic_logo)
-                .setContentTitle(remoteMessage.data["title"])
-                .setContentText(remoteMessage.data["content"])
                 .setAutoCancel(true)
                 .setSound(defaultSoundUri)
                 .setContentIntent(pendingIntent)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(channelId, "OApp Push Notification", NotificationManager.IMPORTANCE_DEFAULT)
+            val channel = NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT)
             notificationManager.createNotificationChannel(channel)
         }
 
-        notificationManager.notify(1 /* ID of notification */, notificationBuilder.build())
+        notificationManager.notify(generateNotificationID(), notificationBuilder.build())
+    }
+
+    private fun generateNotificationID(): Int {
+        val now = Date()
+        return Integer.parseInt(SimpleDateFormat("yyMMddHHmmssZ", Locale.getDefault()).format(now))
     }
 
 }
