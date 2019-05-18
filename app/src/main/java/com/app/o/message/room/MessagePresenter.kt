868cc87f76ec194ffa6df5e301ff67273e3ea218
Reza Rachman <reza.rachman@traveloka.com>
2019-05-18 13:00:24 +0700

    Handle errors in MessagePresenter.

--- app/src/main/java/com/app/o/message/room/MessagePresenter.kt
+++ app/src/main/java/com/app/o/message/room/MessagePresenter.kt
@@ -6,29 +6,36 @@ import com.app.o.base.presenter.OAppPresenter
 import com.app.o.base.service.OAppSubmitMessageService
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 class MessagePresenter(private val commentView: OAppSubmitMessageService,
                        private val compositeDisposable: CompositeDisposable) : OAppPresenter() {
 
     fun postReplyComment(spec: CommentSpec) {
-        compositeDisposable.add(APIRepository.create().submitReplyComment(spec, getHeaderAuth())
-                .subscribeOn(Schedulers.io())
-                .compose {
-                    it.observeOn(AndroidSchedulers.mainThread())
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    commentView.onMessageBeingProcessed()
-                }
-                .doOnError{
-                    commentView.onMessageNotSent()
-                    it.printStackTrace()
-                }
-                .subscribe(Consumer {
-                    commentView.onMessageSent(it)
-                }))
+        try {
+            compositeDisposable.add(APIRepository.create().submitReplyComment(spec, getHeaderAuth())
+                    .subscribeOn(Schedulers.io())
+                    .compose {
+                        it.observeOn(AndroidSchedulers.mainThread())
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe {
+                        commentView.onMessageBeingProcessed()
+                    }
+                    .doOnError{
+                        commentView.onMessageNotSent()
+                        it.printStackTrace()
+                    }
+                    .subscribe { submitCommentResponse, throwable ->
+                        val succeed = { commentView.onMessageSent(submitCommentResponse) }
+                        val failed = { commentView.onMessageNotSent() }
+
+                        subscriberHandler(throwable, succeed, failed)
+                    }
+            )
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
 }
