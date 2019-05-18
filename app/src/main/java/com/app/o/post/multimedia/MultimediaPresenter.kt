4776ec3e369faa11620edfe21c9019ca1cfbea8a
Reza Rachman <reza.rachman@traveloka.com>
2019-05-18 13:03:45 +0700

    Handle errors in MultimediaPresenter.

--- app/src/main/java/com/app/o/post/multimedia/MultimediaPresenter.kt
+++ app/src/main/java/com/app/o/post/multimedia/MultimediaPresenter.kt
@@ -7,7 +7,6 @@ import com.app.o.base.service.OAppViewService
 import com.app.o.shared.util.OAppUtil
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -25,24 +24,36 @@ abstract class MultimediaPresenter(
             latitude: RequestBody,
             longitude: RequestBody,
             content: RequestBody) {
+        try {
+            compositeDisposable.add(APIRepository.create().createPost(files, title, subtitle, type, latitude, longitude, content, getHeaderAuth())
+                    .subscribeOn(Schedulers.io())
+                    .compose {
+                        it.observeOn(AndroidSchedulers.mainThread())
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe {
+                        view.showLoading()
+                    }
+                    .doOnError{
+                        view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        it.printStackTrace()
+                    }
+                    .subscribe { createdPostResponse, throwable ->
+                        val succeed = {
+                            view.onDataResponse(createdPostResponse)
+                            view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
+                        }
 
-        compositeDisposable.add(APIRepository.create().createPost(files, title, subtitle, type, latitude, longitude, content, getHeaderAuth())
-                .subscribeOn(Schedulers.io())
-                .compose {
-                    it.observeOn(AndroidSchedulers.mainThread())
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    view.showLoading()
-                }
-                .doOnError{
-                    view.hideLoading(OAppUtil.ON_FINISH_FAILED)
-                    it.printStackTrace()
-                }
-                .subscribe(Consumer {
-                    view.onDataResponse(it)
-                    view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
-                }))
+                        val failed = {
+                            view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        }
+
+                        subscriberHandler(throwable, succeed, failed)
+                    }
+            )
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
 }
