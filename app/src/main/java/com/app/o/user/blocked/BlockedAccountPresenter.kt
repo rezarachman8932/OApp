2f4a46fd722f21d8b4918447e2ae19d20e3fd863
Reza Rachman <reza.rachman@traveloka.com>
2019-05-18 12:41:23 +0700

    Handle errors in BlockedAccountPresenter.

--- app/src/main/java/com/app/o/user/blocked/BlockedAccountPresenter.kt
+++ app/src/main/java/com/app/o/user/blocked/BlockedAccountPresenter.kt
@@ -9,7 +9,6 @@ import com.app.o.shared.util.OAppUtil
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 class BlockedAccountPresenter(private val view: OAppViewService<BlockedUserResponse>,
@@ -17,50 +16,69 @@ class BlockedAccountPresenter(private val view: OAppViewService<BlockedUserRespo
                               private val compositeDisposable: CompositeDisposable) : OAppPresenter() {
 
     fun getBlockedUsers() {
-        compositeDisposable.add(APIRepository.create().getBlockedUsers(getHeaderAuth())
-                .subscribeOn(Schedulers.io())
-                .compose {
-                    it.observeOn(AndroidSchedulers.mainThread())
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    view.showLoading()
-                }
-                .onErrorResumeNext {
-                    view.hideLoading(OAppUtil.ON_FINISH_FAILED)
-                    Single.error(it)
-                }
-                .doOnError{
-                    view.hideLoading(OAppUtil.ON_FINISH_FAILED)
-                    it.printStackTrace()
-                }
-                .subscribe(Consumer {
-                    view.onDataResponse(it)
-                    view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
-                }))
+        try {
+            compositeDisposable.add(APIRepository.create().getBlockedUsers(getHeaderAuth())
+                    .subscribeOn(Schedulers.io())
+                    .compose {
+                        it.observeOn(AndroidSchedulers.mainThread())
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe {
+                        view.showLoading()
+                    }
+                    .onErrorResumeNext {
+                        view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        Single.error(it)
+                    }
+                    .doOnError{
+                        view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        it.printStackTrace()
+                    }
+                    .subscribe { blockedUserResponse, throwable ->
+                        val succeed = {
+                            view.onDataResponse(blockedUserResponse)
+                            view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
+                        }
+
+                        val failed = {
+                            view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        }
+
+                        subscriberHandler(throwable, succeed, failed)
+                    })
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
     fun unBlockedUser(spec: UserBlockingSpec) {
-        compositeDisposable.add(APIRepository.create().unBlockedUser(spec, getHeaderAuth())
-                .subscribeOn(Schedulers.io())
-                .compose {
-                    it.observeOn(AndroidSchedulers.mainThread())
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    unBlockCallback.onProgress()
-                }
-                .onErrorResumeNext {
-                    unBlockCallback.onFailed()
-                    Single.error(it)
-                }
-                .doOnError{
-                    unBlockCallback.onFailed()
-                    it.printStackTrace()
-                }
-                .subscribe(Consumer {
-                    unBlockCallback.onSucceed(it)
-                }))
+        try {
+            compositeDisposable.add(APIRepository.create().unBlockedUser(spec, getHeaderAuth())
+                    .subscribeOn(Schedulers.io())
+                    .compose {
+                        it.observeOn(AndroidSchedulers.mainThread())
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe {
+                        unBlockCallback.onProgress()
+                    }
+                    .onErrorResumeNext {
+                        unBlockCallback.onFailed()
+                        Single.error(it)
+                    }
+                    .doOnError{
+                        unBlockCallback.onFailed()
+                        it.printStackTrace()
+                    }
+                    .subscribe { userBlockingResponse, throwable ->
+                        val succeed = { unBlockCallback.onSucceed(userBlockingResponse) }
+                        val failed = { unBlockCallback.onFailed() }
+
+                        subscriberHandler(throwable, succeed, failed)
+                    })
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
 }
