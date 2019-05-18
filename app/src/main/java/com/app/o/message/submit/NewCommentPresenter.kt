cec21f9bad6602a1afd1d1a9e073ebfe99a39025
Reza Rachman <reza.rachman@traveloka.com>
2019-05-18 13:07:07 +0700

    Handle errors in NewCommentPresenter.

--- app/src/main/java/com/app/o/message/submit/NewCommentPresenter.kt
+++ app/src/main/java/com/app/o/message/submit/NewCommentPresenter.kt
@@ -8,30 +8,42 @@ import com.app.o.base.service.OAppViewService
 import com.app.o.shared.util.OAppUtil
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
-import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 class NewCommentPresenter(private val view: OAppViewService<SubmitCommentResponse>,
                           private val compositeDisposable: CompositeDisposable) : OAppPresenter() {
 
     fun submitNewComment(spec: CommentSpec) {
-        compositeDisposable.add(APIRepository.create().submitNewComment(spec, getHeaderAuth())
-                .subscribeOn(Schedulers.io())
-                .compose {
-                    it.observeOn(AndroidSchedulers.mainThread())
-                }
-                .observeOn(AndroidSchedulers.mainThread())
-                .doOnSubscribe {
-                    view.showLoading()
-                }
-                .doOnError{
-                    view.hideLoading(OAppUtil.ON_FINISH_FAILED)
-                    it.printStackTrace()
-                }
-                .subscribe(Consumer {
-                    view.onDataResponse(it)
-                    view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
-                }))
+        try {
+            compositeDisposable.add(APIRepository.create().submitNewComment(spec, getHeaderAuth())
+                    .subscribeOn(Schedulers.io())
+                    .compose {
+                        it.observeOn(AndroidSchedulers.mainThread())
+                    }
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .doOnSubscribe {
+                        view.showLoading()
+                    }
+                    .doOnError{
+                        view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        it.printStackTrace()
+                    }
+                    .subscribe { submitCommentResponse, throwable ->
+                        val succeed = {
+                            view.onDataResponse(submitCommentResponse)
+                            view.hideLoading(OAppUtil.ON_FINISH_SUCCEED)
+                        }
+
+                        val failed = {
+                            view.hideLoading(OAppUtil.ON_FINISH_FAILED)
+                        }
+
+                        subscriberHandler(throwable, succeed, failed)
+                    }
+            )
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+        }
     }
 
 }
